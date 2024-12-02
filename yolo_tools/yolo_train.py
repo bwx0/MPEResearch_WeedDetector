10638edc3641ce69ddd0ae209972e5d4841f003e
bwx <bwx8369@gmail.com>
2024-12-03 01:39:27 +0800

    update readme; adjustable imgsz

--- yolo_tools/yolo_train.py
+++ yolo_tools/yolo_train.py
@@ -4,7 +4,7 @@ import testing.test_init
 model = YOLO("../yolov8n.pt")
 
 if __name__ == '__main__':
-    model.train(data="../dataset/test3_2_final/data.yaml", epochs=80, imgsz=640, cos_lr=True)
+    model.train(data="../dataset/test3_final/data.yaml", epochs=80, imgsz=480, cos_lr=True)
     model.val()
 
 
